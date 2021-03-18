0074be42acd308b212c0547340ef508aafeee2f6
whl <whl0506@163.com>
2021-03-18 19:43:32 +0800

    修改docker-maven-plugin中pom文件信息

--- document/sh/mall-admin.sh
+++ document/sh/mall-admin.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 app_name='mall-admin'
-
-#将打包好的jar项目，移到/opt/data/build目录
-#/var/jenkins_home/workspace/SpringBoot_AutoTest/target/jenkins-0.0.1-SNAPSHOT.jar构建好的jar路径
-echo '----move jar file----'
-mv /var/lib/jenkins/workspace/${app_name}/${app_name}/target/${app_name}-1.0-SNAPSHOT.jar /opt/data/build
-#切换目录到/opt/data/build
-cd /opt/data/build
-#执行构建Dockerfile命令
-echo '----build Dockerfile----'
-docker build /var/jenkins_home/workspace/${app_name}/${app_name}/ -t ${app_name}
-
 docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
